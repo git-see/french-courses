368abe039a64c7e4bd6683dc58b336052fda8a13
John Doe <johndoe@exemple.com>
2024-08-19 17:24:53 +0200

    routing- NavLink/condition for css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,27 @@
+import { NavLink } from "react-router-dom";
+
 export default function Menu() {
   return (
     <div className="menu">
       <ul>
-        <li>Home</li>
-        <li>Add course</li>
-        <li>All courses</li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/add"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >Add course</NavLink>
+        </li>
+        <li>
+          <NavLink to="/list"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >All courses</NavLink>
+        </li>
       </ul>
     </div>
   );
